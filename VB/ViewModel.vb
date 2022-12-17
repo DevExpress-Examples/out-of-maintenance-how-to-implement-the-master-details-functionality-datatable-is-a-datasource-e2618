ed5ce85d69ee0495c5ec9b6617d5e85415f1ed10
DevExpressExampleBot <fakeEmail>
2022-12-17 06:59:48 +0400

    Source auto update [skip ci]

--- VB/ViewModel.vb
+++ VB/ViewModel.vb
@@ -1,11 +1,9 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Data
-Imports System.Linq
-Imports System.Text
 
 Namespace MasterDetailInside
+
     Public Class ViewModel
+
         Public Sub New()
             ParentData = New DataTable()
             ParentData.Columns.Add("Id", GetType(Integer))
@@ -13,14 +11,16 @@ Namespace MasterDetailInside
             ChildData = New DataTable()
             ChildData.Columns.Add("ParentId", GetType(Integer))
             ChildData.Columns.Add("Text", GetType(String))
-            For i As Integer = 0 To 99
+            For i As Integer = 0 To 100 - 1
                 ParentData.Rows.Add(i, "Master" & i)
-                For j As Integer = 0 To 49
+                For j As Integer = 0 To 50 - 1
                     ChildData.Rows.Add(i, "Detail" & j & " Master" & i)
-                Next j
-            Next i
+                Next
+            Next
         End Sub
-        Public Property ParentData() As DataTable
-        Public Property ChildData() As DataTable
+
+        Public Property ParentData As DataTable
+
+        Public Property ChildData As DataTable
     End Class
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,18 +1,17 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Windows
-Imports DevExpress.Xpf.Grid
 Imports System.Globalization
 Imports System.Windows.Data
 Imports System.Windows.Markup
 Imports System.Data
 
 Namespace MasterDetailInside
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 
@@ -20,15 +19,15 @@ Namespace MasterDetailInside
         Inherits MarkupExtension
         Implements IMultiValueConverter
 
-        Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IMultiValueConverter.Convert
+        Public Function Convert(ByVal values As Object(), ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IMultiValueConverter.Convert
             Dim masterRowId = values(0)
-            Dim childData = DirectCast(values(1), DataTable)
+            Dim childData = CType(values(1), DataTable)
             Dim childView = New DataView(childData)
             childView.RowFilter = String.Format("ParentId = '{0}'", masterRowId.ToString())
             Return childView
         End Function
 
-        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes As Type(), ByVal parameter As Object, ByVal culture As CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
             Throw New NotImplementedException()
         End Function
 
